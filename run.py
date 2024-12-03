a19ed0203a83a35411fdd22f50976672d8392b79
Lucas Soares <lucas_1801@yahoo.com.br>
2024-12-03 13:40:57 -0300

    config: adding pre-commit

---
+++ run.py
@@ -0,0 +1,10 @@
+'''
+docstring que o pylint exige
+'''
+#"--rcfile=.pylintrc", # Link to your config file
+def something():
+    '''
+    detalhe sobre a função
+    '''
+    print('hello world')
+    
